eb0f8db2a2a883e354ffcd9ffc9aa852977ef5ba
milan0827 <milanrokaya2014@gmail.com>
2024-01-24 14:09:18 +0545

    style: Button style fixed

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -5,25 +5,20 @@ type Props = {
   icon?: ReactNode;
   label: string;
   btnType: "primary" | "secondary" | "tertiary" | "quartinary";
-  labelType: "primary" | "secondary";
   className?: string;
   size?: "sm" | "md" | "xsm";
-};
-
-type StyleProp = {
-  primary: string;
-  secondary: string;
-  tertiary?: string;
 };
 
 const button = tv({
   base: "flex justify-center items-center gap-8 rounded-btn px-[80px] py-5 border-2",
   variants: {
     color: {
-      primary: "bg-blue-primary-color-400",
-      secondary: "border-blue-primary-color-400 border-blue-primary-color-400",
-      tertiary: "bg-blue-primary-color-400",
-      quartinary: "bg-blue-primary-color-200/40 border-blue-primary-color-400",
+      primary: "bg-blue-primary-color-400 text-grey-primary-color-100",
+      secondary:
+        "border-blue-primary-color-400 border-blue-primary-color-400 text-blue-primary-color-400",
+      tertiary: "bg-blue-primary-color-400 text-grey-primary-color-100",
+      quartinary:
+        "bg-blue-primary-color-200/40 border-blue-primary-color-400 text-blue-primary-color-400",
     },
     size: {
       xsm: "py-1 px-2 gap-2",
@@ -33,27 +28,13 @@ const button = tv({
   },
 });
 
-export const Button = ({
-  icon,
-  label,
-  btnType,
-  labelType,
-  className,
-  size,
-}: Props) => {
-  const textStyle: StyleProp = {
-    primary: "text-blue-primary-color-400",
-    secondary: "text-grey-primary-color-100",
-  };
-
+export const Button = ({ icon, label, btnType, className, size }: Props) => {
   return (
     <button
       className={`${button({ color: btnType, size: size, class: className })}`}
     >
-      {icon ? <span>{icon}</span> : ""}
-      <span className={`${textStyle[labelType]} text-xl font-bold`}>
-        {label}
-      </span>
+      {icon ? <span>{icon}</span> : null}
+      <span className={` text-xl font-bold`}>{label}</span>
     </button>
   );
 };
